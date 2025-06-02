8ef23134e236c630aa2cb2b3fd71d536734d5d1b
Sanggyukang915 <Sanggyukang915@gmail.com>
2025-06-02 12:22:31 -0400

    add context db

--- data/document.ts
+++ data/document.ts
@@ -1,11 +1,11 @@
 "use server";
 
-import {db} from "@/lib/db"
+import { db } from "@/lib/db"
 import { currentUser } from "@/lib/auth";
 
-export const createDocument = async (name: string)=>{
+export const createDocument = async (name: string) => {
     const user = await currentUser();
-    if(!user?.id) throw new Error("Unauthorized");
+    if (!user?.id) throw new Error("Unauthorized");
 
     const document = await db.document.create({
         data: {
@@ -16,22 +16,63 @@ export const createDocument = async (name: string)=>{
     return document;
 }
 
-export const updateDocument = async (id: string, name: string)=>{
+export const updateDocument = async (id: string, name: string) => {
     return await db.document.update({
-        where: {id},
-        data: {name},
+        where: { id },
+        data: { name },
     });
 };
 
-export const deleteDocument = async (id: string)=>{
+export const deleteDocument = async (id: string) => {
     return await db.document.delete({
-        where: {id},
+        where: { id },
     });
 };
 
-export const getDocument = async (id:string) =>{
+export const getDocument = async (id: string) => {
     const document = await db.document.findUnique({
-        where: {id},
+        where: { id },
+        include: {
+            content: true,
+        }
     });
     return document;
+}
+
+export const createDocumentContent = async (id: string) => {
+    return await db.document.update({
+        where: { id },
+        data:{
+            content: {
+                create: [
+                    {content: ""},
+                ]
+            }
+        }
+    })
+}
+export const updateDocumentContent = async (id: string, newContent: string) => {
+    return await db.document.update({
+        where: { id },
+        data:{
+            content: {
+                create: [
+                    {content: newContent},
+                ]
+            }
+        }
+    })
+}
+
+export const deleteDocumentContent = async (id: string, newContent: string) => {
+    return await db.document.update({
+        where: { id },
+        data:{
+            content: {
+                create: [
+                    {content: newContent},
+                ]
+            }
+        }
+    })
 }
